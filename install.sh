d1a3f00d8f6ee50365550d0130710999733ad809
b <a>
2019-01-25 06:25:37 +0000

    add

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y python-pygame
 wget https://storage.googleapis.com/golang/go1.8.linux-armv6l.tar.gz
 sudo tar -C /usr/local/ -xzf go1.8.linux-armv6l.tar.gz
-mkdir go/src/github.com/rinking
+mkdir -p go/src/github.com/rinking
 mv diff.patch go/src/github.com/rinking
 cd go/src/github.com/riking/
 git clone https://github.com/riking/joycon.git
