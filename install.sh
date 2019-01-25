b5f30a5cad18b0756eeed9c2c06c5b3d862674a1
b <a>
2019-01-25 06:06:34 +0000

    add file

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
+#!/bin/sh
+cd `dirname $0`
+export GOPATH=$(pwd)/go
+echo $GOPATH
 sudo apt-get update
 sudo apt-get install -y python-pygame
+wget https://storage.googleapis.com/golang/go1.8.linux-armv6l.tar.gz
+sudo tar -C /usr/local/ -xzf go1.8.linux-armv6l.tar.gz
+mv diff.patch go/src/github.com/rinking
+cd go/src/github.com/riking/
+git clone https://github.com/riking/joycon.git
+cd joicon/
+patch -p1 < ../diff.patch
+cd prog4
+/usr/local/go/bin/go get ./...
